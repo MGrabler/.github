11aef7c1535b37a06b827757f4008ae67bf3cdcf
MGrabler <gab19925@spengergasse.at>
2022-01-21 02:52:27 +0100

    Change Header.java

--- templates/Java/File Header.java
+++ templates/Java/File Header.java
@@ -2,7 +2,6 @@
  * The class {@code ${NAME}} Translates strings in matrix form into
  * <p>different types of a matrix like {@code Boolean}, {@code Integer}, {@code Double}.
  *
- * @created ${DATE}, ${DAY_NAME_FULL}
  * @author ${USER}
  * @version 0.1
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "") * @see ${PACKAGE_NAME}
@@ -10,3 +9,4 @@
  * @see String
  * @since 1.0
  */
+// @created ${DATE}, ${DAY_NAME_FULL}
